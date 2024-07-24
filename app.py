f36b9962fc17da8d32ff96271d08b55637a0f0a5
sahil4370125 <sa4370125@alphacollege.me>
2024-07-24 17:15:34 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def respond(
     temperature,
     top_p,
 ):
-    system_message = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise."
+    system_message = "you are a skilled Italian chef, you are here to provide expert guidance on cooking methodologies and step-by-step recipes. on the art of grilling a steak, mastering the delicate techniques of Italian pastry making, or craving a comforting dish"
     messages = [{"role": "system", "content": system_message}]
 
     for val in history:
@@ -46,7 +46,7 @@ For information on how to customize the ChatInterface, peruse the gradio docs: h
 demo = gr.ChatInterface(
     respond,
     additional_inputs=[
-        gr.Textbox(value = "You are a good listener. You advise relaxation exercises, suggest avoiding negative thoughts, and guide through steps to manage stress. Discuss what's on your mind, or ask me for a quick relaxation exercise.", label="System message"),
+        gr.Textbox(value = "you are a skilled Italian chef, you are here to provide expert guidance on cooking methodologies and step-by-step recipes. on the art of grilling a steak, mastering the delicate techniques of Italian pastry making, or craving a comforting dish", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
         gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
         gr.Slider(
@@ -59,11 +59,11 @@ demo = gr.ChatInterface(
     ],
 
     examples = [ 
-        ["I feel overwhelmed with work."],
-        ["Can you guide me through a quick meditation?"],
-        ["How do I stop worrying about things I can't control?"]
+        ["How to perfectly grill a steak like they do in Italy?"],
+        ["How do I elevate a simple tomato sauce to make it more flavorful?"],
+        ["Can you recommend a traditional Italian pasta recipe?"]
     ],
-    title = 'Calm Mate 🕊️'
+    title = 'Italian_Chef'
 )
 
 
